1c4574a2e2fa4dbb3f00b8d25a43ebc4d11a620b
Kelvie Wong <kelvie@kelvie.ca>
2022-07-04 22:17:21 -0700

    Re-haul to actually write to MQTT

--- custom/acurite-00986.h
+++ custom/acurite-00986.h
@@ -5,12 +5,15 @@
 
 #include "esphome.h"
 
+#include <cstdio>
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
 
-class Acurite00986Sensor : public TextSensor, public Component {
+class Acurite00986Component : public CustomMQTTDevice, public Component {
 public:
-  Acurite00986Sensor(gpio_num_t pin) {
+  // Will add <sensor_num>-<sensor_id> to the topic prefix
+  Acurite00986Component(gpio_num_t pin, const std::string &topic_prefix)
+      : topic_prefix_(topic_prefix) {
     auto idfpin = new esp32::IDFInternalGPIOPin;
     idfpin->set_pin(pin);
     // 20mA
@@ -30,14 +33,14 @@ public:
     msg_index_ = 0;
     sync_cycles_ = 0;
     edge_after_sync_ = true;
-    pin_->attach_interrupt(Acurite00986Sensor::gpio_isr, this,
+    pin_->attach_interrupt(Acurite00986Component::gpio_isr, this,
                            gpio::INTERRUPT_ANY_EDGE);
     last_edge_ = micros();
   }
 
   int parse_temp_celsius(char n) {
     int magnitude_f = n & (~0x80);
-    magnitude_f *=  n & 0x80 ? -1 : 1;
+    magnitude_f *= n & 0x80 ? -1 : 1;
 
     return (magnitude_f - 32) * 100 / 180;
   }
@@ -45,11 +48,19 @@ public:
   void loop() override {
     char msg[5];
     while (xQueueReceive(queue_, msg, 0) == pdTRUE) {
-      // TODO: place these into sensor outputs for esphome, to send to MQTT
-      // TODO: create mqtt topic names based on sensor IDs
-      ESP_LOGD(TAG, "Sensor %d (id: %.2x%.2x) %d°C.%s",
-               msg[3] & 1 ? 2 : 1, msg[1], msg[2], parse_temp_celsius(msg[0]),
+      ESP_LOGD(TAG, "Sensor %d (id: %.2x%.2x) %d°C.%s", msg[3] & 1 ? 2 : 1,
+               msg[1], msg[2], parse_temp_celsius(msg[0]),
                msg[3] & 2 ? " Low battery." : "");
+
+      // Topic name:
+      // <topic_prefix_>-<sensor_num>-<sensor_id>
+      char topic_suffix[] = "-1-abcd";
+      snprintf(topic_suffix, sizeof(topic_suffix), "%d-%.2x%.2x",
+               msg[3] & 1 ? 2 : 1, msg[1], msg[2]);
+      this->publish_json(topic_prefix_ + topic_suffix, [=](JsonObject root) {
+        root["temp_c"] = parse_temp_celsius(msg[0]);
+        root["low_battery"] = msg[3] & 2 ? 1 : 0;
+      });
     }
 
     int log_sync_cycle;
@@ -78,10 +89,10 @@ protected:
   }
 
   // Interrupt handler, so let's keep it short and only access volatiles.
-  static void IRAM_ATTR gpio_isr(Acurite00986Sensor *sensor) {
+  static void IRAM_ATTR gpio_isr(Acurite00986Component *c) {
     const uint32_t now = micros();
-    const bool rising = sensor->isr_pin_.digital_read();
-    volatile char *cur = sensor->msg_ + sensor->msg_index_;
+    const bool rising = c->isr_pin_.digital_read();
+    volatile char *cur = c->msg_ + c->msg_index_;
 
     // For sending values to the main loop to log for debugging
     int debug_send;
@@ -89,13 +100,13 @@ protected:
     // TODO: decide on skip if necessary to save cycles
 
     // We are not synced util we have seen 7 edges spaced sync_width_us apart
-    if (sensor->sync_cycles_ < 7) {
-      if (sensor->compare_pulse(now - sensor->last_edge_, sync_width_us_)) {
-        sensor->sync_cycles_++;
+    if (c->sync_cycles_ < 7) {
+      if (c->compare_pulse(now - c->last_edge_, sync_width_us_)) {
+        c->sync_cycles_++;
         goto end;
       }
 
-      sensor->sync_cycles_ = 0;
+      c->sync_cycles_ = 0;
       goto end;
     }
 
@@ -109,61 +120,62 @@ protected:
     }
 
     // On the first rising edge (after syncing), just record the edge.
-    if (sensor->edge_after_sync_) {
-      sensor->edge_after_sync_ = false;
+    if (c->edge_after_sync_) {
+      c->edge_after_sync_ = false;
       goto end;
     }
 
     // For debugging, to send various values from the ISR
     // debug_send = now;
-    // xQueueSendFromISR(sensor->log_queue_, &debug_send, NULL);
+    // xQueueSendFromISR(c->log_queue_, &debug_send, NULL);
 
     // At this point, we're measuring rising to rising for logic highs and
     // lows. Bytes are sent LSB first
-    if (sensor->compare_pulse(now - sensor->last_edge_, logic_high_width_us_)) {
+    if (c->compare_pulse(now - c->last_edge_, logic_high_width_us_)) {
       *cur = (*cur >> 1) + 0x80;
-      sensor->bit_index_++;
-    } else if (sensor->compare_pulse(now - sensor->last_edge_,
-                                     logic_low_width_us_)) {
+      c->bit_index_++;
+    } else if (c->compare_pulse(now - c->last_edge_, logic_low_width_us_)) {
       *cur = *cur >> 1;
-      sensor->bit_index_++;
+      c->bit_index_++;
     } else {
       // Not high, not low, probably noise.
-      sensor->msg_index_ = 0;
-      sensor->bit_index_ = 0;
-      sensor->sync_cycles_ = 0;
-      sensor->edge_after_sync_ = true;
+      c->msg_index_ = 0;
+      c->bit_index_ = 0;
+      c->sync_cycles_ = 0;
+      c->edge_after_sync_ = true;
       goto end;
     }
 
     // Move onto next byte if 8 bits have been written
-    if (sensor->bit_index_ > 7) {
-      sensor->msg_index_++;
-      sensor->bit_index_ = 0;
+    if (c->bit_index_ > 7) {
+      c->msg_index_++;
+      c->bit_index_ = 0;
     }
 
     // Send the full message after all 5 messages are received
-    if (sensor->msg_index_ >= 5) {
+    if (c->msg_index_ >= 5) {
       char msg[5];
 
-      // memcpy(msg, sensor->msg_, 5);
+      // memcpy(msg, c->msg_, 5);
       for (int i = 0; i < sizeof(msg); i++) {
-        msg[i] = sensor->msg_[i];
+        msg[i] = c->msg_[i];
       }
 
-      xQueueSendFromISR(sensor->queue_, msg, NULL);
-      sensor->msg_index_ = 0;
-      sensor->bit_index_ = 0;
-      sensor->sync_cycles_ = 0;
-      sensor->edge_after_sync_ = true;
+      xQueueSendFromISR(c->queue_, msg, NULL);
+      c->msg_index_ = 0;
+      c->bit_index_ = 0;
+      c->sync_cycles_ = 0;
+      c->edge_after_sync_ = true;
     }
 
   end:
-    sensor->last_edge_ = now;
+    c->last_edge_ = now;
     return;
   }
 
 private:
+  const std::string topic_prefix_;
+
   InternalGPIOPin *pin_ = nullptr;
   ISRInternalGPIOPin isr_pin_;
 

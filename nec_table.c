15e8b88e5d769bf0921235e05b6a14e847cdd223
Kelvie Wong <kelvie@kelvie.ca>
2022-09-25 15:07:04 -0700

    Fix nec sending
    Looks like the format changed byte order.

--- nec_table.c
+++ nec_table.c
@@ -3,9 +3,10 @@
 
 int nec_format(unsigned char b) {
   // Reverse bits as NEC sends LSB first
-  b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-  b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-  b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+  // No longer needed as of esphome 2021.12
+  // b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+  // b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+  // b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
 
   // Calculate inverse in lower bytes
   return (b << 8) + (~b & 0xff);
